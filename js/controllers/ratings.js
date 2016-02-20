0c500e34e230a0f413add096cb410bb3952a38fb
schrealex <cer34ll@gmail.com>
2016-02-20 17:33:40 +0100

    Replaced ratings with different ratings handler

--- js/controllers/ratings.js
+++ js/controllers/ratings.js
@@ -2,83 +2,88 @@
 
 var ratingsController = angular.module('ratingsController', ['movieServices']);
 
-ratingsController.controller('RatingsController', ['$scope', '$routeParams', function ($scope) {
-    this.rating = 1;
+ratingsController.controller('RatingsController', ['$scope',
+    function ($scope) {
+        $scope.starRating = 0;
+        $scope.hoverRating = 0;
 
-    $scope.mouseHover1 = function (param) {
-       console.log('mouseHover(' + param + ')');
-        $scope.hoverRating1 = param;
-    };
+        $scope.click = function (param) {
+            console.log('Click(' + param + ')');
+        };
 
-    $scope.mouseLeave1 = function (param) {
-        console.log('mouseLeave(' + param + ')');
-        $scope.hoverRating1 = param + '*';
-    };
-}]).directive('starRating', starRating);
+        $scope.mouseHover = function (param) {
+            console.log('mouseHover(' + param + ')');
+            $scope.hoverRating1 = param;
+        };
 
-function starRating() {
+        $scope.mouseLeave = function (param) {
+            console.log('mouseLeave(' + param + ')');
+            $scope.hoverRating1 = param + '*';
+        };
+    }
+]);
+
+ratingsController.directive('starRating', function () {
     return {
-        restrict: 'EA',
-        template: '<ul class="star-rating" ng-class="{readonly: readonly}">' +
-            '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-click="toggle($index)" ng-mouseenter="isolatedMouseHover($index + 1)"' +
-            '  ng-mouseleave="isolatedMouseLeave($index + 1)">' +
-            '  <i class="fa fa-star"></i></li>' +
-            '</ul>',
         scope: {
-            ratingValue: '=ngModel',
-            max: '=?', // optional (default is 5)
-            onRatingSelect: '&?',
-            readonly: '=?',
-        	click: "&",
+            rating: '=',
+            maxRating: '@',
+            readOnly: '@',
+            click: "&",
             mouseHover: "&",
             mouseLeave: "&"
         },
-        link: function(scope, element, attributes) {
-            if (scope.max == undefined) {
-                scope.max = 5;
-            }
+        restrict: 'EA',
+        template:
+            "<div style='display: inline-block; margin: 0px; padding: 0px; cursor:pointer;' ng-repeat='idx in maxRatings track by $index'> \
+                    <img ng-src='{{((hoverValue + _rating) <= $index) && \"http://www.codeproject.com/script/ratings/images/star-empty-lg.png\" || \"http://www.codeproject.com/script/ratings/images/star-fill-lg.png\"}}' \
+                    ng-Click='isolatedClick($index + 1)' \
+                    ng-mouseenter='isolatedMouseHover($index + 1)' \
+                    ng-mouseleave='isolatedMouseLeave($index + 1)'></img> \
+            </div>",
+        compile: function (element, attrs) {
+            if (!attrs.maxRating || (Number(attrs.maxRating) <= 0)) {
+                attrs.maxRating = '5';
+            };
+        },
+        controller: function ($scope, $element, $attrs) {
+            $scope.maxRatings = [];
 
-            function updateStars() {
-                scope.stars = [];
-                for (var i = 0; i < scope.max; i++) {
-                    scope.stars.push({
-                        filled: i < scope.ratingValue
-                    });
-                }
+            for (var i = 1; i <= $scope.maxRating; i++) {
+                $scope.maxRatings.push({});
             };
-            scope.toggle = function(index) {
-                if (scope.readonly == undefined || scope.readonly === false) {
-                    scope.ratingValue = index + 1;
-                    scope.onRatingSelect({
-                        rating: index + 1
-                    });
-                }
-            };
-            scope.$watch('ratingValue', function(oldValue, newValue) {
-                if (newValue) {
-                    updateStars();
-                }
-            });
-            
-            scope.isolatedMouseHover = function (param) {
-                scope._rating = 0;
-                scope.ratingValue = param;
-                scope.mouseHover({
-                	param: param
+
+            $scope._rating = $scope.rating;
+
+            $scope.isolatedClick = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope.rating = $scope._rating = param;
+                $scope.hoverValue = 0;
+                $scope.click({
+                    param: param
                 });
-        	};
+            };
 
-			scope.isolatedMouseLeave = function (param) {
-			    if (scope.readOnly == 'true') {
-			    	return;
-			    }
-		
-			    scope._rating = scope.rating;
-			    scope.ratingValue = 0;
-			    scope.mouseLeave({
-			    	param: param
-			    });
-			};
+            $scope.isolatedMouseHover = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope._rating = 0;
+                $scope.hoverValue = param;
+                $scope.mouseHover({
+                    param: param
+                });
+            };
+
+            $scope.isolatedMouseLeave = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope._rating = $scope.rating;
+                $scope.hoverValue = 0;
+                $scope.mouseLeave({
+                    param: param
+                });
+            };
         }
     };
-}    
+});
